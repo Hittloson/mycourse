6d828466a23d14e6291b245e69369a84658ba9f7
Hittloson <hittlo86@gmail.com>
2024-02-22 01:26:40 +0000

    Contact Card

---
+++ Contact-App/contact/src/App.jsx
@@ -0,0 +1,36 @@
+
+import { useEffect, useState } from 'react';
+import './App.css'
+import ContactCard from './ContactCard'
+
+function App() {
+  
+  const [results, setResults]=useState([]);
+  useEffect(()=>{
+    fetch("https://randomuser.me/api/?results=5")
+  .then(response =>response.json())
+  .then(data => {
+    console.log(data)
+    setResults(data.results)
+  });
+  },[])
+  
+  return (
+    <div>
+      {results.map((result, index)=>{
+        return(
+          <ContactCard key={index}
+            avatarUrl = {result.picture.large}
+            name = {result.name.first}
+            email = {result.email}
+            age = {result.dob.age}
+           />
+        )
+      })
+      }
+    </div>
+    
+  )
+}
+
+export default App
